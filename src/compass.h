7bbc6adf7b111de69f89a37b795265f8279ab39e
Connor <ConnorKirkpatrick3@gmail.com>
2023-02-13 22:19:38 +0000

    Added hardIron corrections

--- src/compass.h
+++ src/compass.h
@@ -1,6 +1,7 @@
 #include "Adafruit_HMC5883_U.h"
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
-double offsetX, offsetY, offsetZ;
+double offsetX=0, offsetY=0, offsetZ=0;
+double mX,mY,mZ;
 void startCompass(){
   if(!mag.begin())
   {
@@ -58,19 +59,21 @@ float getHeading(attitude currentAttitude){
   /* Get a new sensor event */
   sensors_event_t event;
   mag.getEvent(&event);
-
+  mX = event.magnetic.x-offsetX;
+  mY = event.magnetic.y-offsetY;
+  mZ = event.magnetic.z-offsetZ;
   /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
+  Serial.print("X: "); Serial.print(mX); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(mY); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(mZ); Serial.print("  ");Serial.println("uT");
 
 
   //correct for tilt before calculating heading
   //exact use of roll and pitch depends on the device orientation on the drone
   float pitch = DegreestoRads(currentAttitude.pitch);
   float roll = DegreestoRads(currentAttitude.roll);
-  float yHorizontal = event.magnetic.x * cos(pitch) + event.magnetic.y*sin(roll)*sin(pitch) - event.magnetic.z * cos(roll)*sin(pitch);
-  float xHorizontal = event.magnetic.y * cos(roll) + event.magnetic.z*sin(roll);
+  float yHorizontal = mX * cos(pitch) + mY*sin(roll)*sin(pitch) - mZ * cos(roll)*sin(pitch);
+  float xHorizontal = mY * cos(roll) + mZ*sin(roll);
 
   // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
   // Calculate heading when the magnetometer is level, then correct for signs of axis.
